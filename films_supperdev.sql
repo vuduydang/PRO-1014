3493b1ace5f30b44bb07e4b7637db25c28fc31e5
haivdph07968 <58266735+haivdph07968@users.noreply.github.com>
2019-12-02 12:35:23 +0700

    Update films_supperdev.sql

--- films_supperdev.sql
+++ films_supperdev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 30, 2019 lúc 06:46 AM
+-- Thời gian đã tạo: Th12 02, 2019 lúc 06:34 AM
 -- Phiên bản máy phục vụ: 10.4.8-MariaDB
 -- Phiên bản PHP: 7.1.33
 
@@ -61,7 +61,12 @@ CREATE TABLE `films` (
 --
 
 INSERT INTO `films` (`id`, `name`, `series`, `year`, `categories`, `author`, `banner`, `thumbnail`, `content`, `quantity`, `status`, `views`, `url`, `active`) VALUES
-(1, 'doraemon', '', 1985, '', 'Jeffrey Grellman ', '', '', 'Bộ phim kể về một chú mèo máy tên là Doraemon đến từ thế kỉ 22', 0, 'Đang công chiếu', 11, '', 1);
+(1, 'doraemon', 'tập 3', 1985, 'Hành động', 'Jeffrey Grellman ', 'https://farm4.staticflickr.com/3112/2654908420_78be034148.jpg', 'http://4.bp.blogspot.com/-WutdbHhsR9Y/T6zd0dI2ppI/AAAAAAAADKk/Dilt_9bbO2Q/s1600/Anime-ghinh-nen-mobile_by_namkna-blogspot-com_anime30.jpg', 'Bộ phim kể về một chú mèo máy tên là Doraemon đến từ thế kỉ 22', 0, 'Đang công chiếu', 11, '', 1),
+(2, 'doremon tập 2', '', 1965, 'Kimetsu no Yaiba', 'Kimetsu no Yaiba', 'https://www.wb7themes.com/wp-content/uploads/2016/05/Doraemon-v3-theme-s40-320x240-Asha-302-210-205-200-201-C3-00-X2-01.jpg', 'http://hinh-nen.org/images/131025Anh-dong-hoat-hinh-cuu-vui-ve.gif', '50 tập phim hoạt hình Doremon HTV3 lồng tiếng cho màn hình 240 x 320. Thảo luận trong \'Phim - Ảnh\' bắt đầu bởi MrBe09, 24/8/14.', 0, 'công chiếu', 10, '', 0),
+(3, 'chất như nước cất', 'tập 1', 0, 'kinh dị', 'thành long', 'https://tse2.mm.bing.net/th?id=OIP.iCPfirxznZQtWcBE_r7WlgHaEL&pid=Api&P=0&w=314&h=178', 'http://sherly.mobile9.com/download/media/210/doremon_fhm7ds92.jpg', 'kể về chuyến hành trình tìm kho báu huyền thoại One Piece và cuộc phiêu lưu trở thành Vua Hải Tặc - Pirate King của cậu bé Monkey D. Luffy. Để đạt được ước mơ của mình cậu phải đến được tận cùng Đại Hải Trình: vùng biển nguy hiểm và chết chóc nhất thế giới. Chiếc mũ rơm Luffy mang theo bên mình là biểu tượng lịch sử khi nó đã từng thuộc về hai hải tặc vĩ đại: Vua Hải Tặc Gol D. Roger và Tứ Hoàng Shank \"tóc đỏ\".\r\n\r\nLuffy và băng Hải Tặc Mũ Rơm của mình đã băng qua Biển Đông và tiến vào Đại Hải Trình, trải qua nhiều trận đánh khốc liệt và các biến cố kinh hoàng, đánh bại rất nhiều kẻ thù để tiến tới kho báu One Piece. Các nhân vật trong Vua Hải Tặc đều có 1 quá khứ rất đặc biệt và đáng buồn. Ngoài những khả năng đặc biệt trong công việc của mình, bất cứ thành viên nào cũng có khả năng chiến đấu cực tốt. Tính đến thời điểm hiện tại, thủy thủ đoàn chính thức của Luffy gồm 9 người: Thuyền trưởng Monkey D. Luffy, Kiếm sĩ Roronoa Zoro, Hoa tiêu Nami, Xạ thủ Usopp (Sogeking), Đầu bếp Sanji, Bác sĩ Tony Tony Chopper, Nhà khảo cổ Nico Robin, Thợ sửa thuyền Franky, Nhạc công Brook.\r\n\r\nSau 2 năm luyện tập các nhân vật trong băng hải tặc Mũ rơm trở lại mang trong mình hoài bão, tự tin trên con đường chinh phục Kho báu One Piece. Anime Vua hải tặc mang đến nhiều trình tiết gây cấn hơn kèm nhạc phim vua hải tặc sôi động và cuốn hút hơn.\r\nVua Hải Tặc còn ngợi ca tình bạn, tình chiến hữu, sự hi sinh vì bạn bè, phim không chỉ mang lại những tình tiết gay cấn đầy kịch tính, những cuộc chiến đầy thú vị nhưng không kém phần vui nhộn.', 0, '', 0, '', 0),
+(4, 'doremon tập 2', '', 1965, 'Kimetsu no Yaiba', 'Kimetsu no Yaiba', 'https://www.wb7themes.com/wp-content/uploads/2016/05/Doraemon-v3-theme-s40-320x240-Asha-302-210-205-200-201-C3-00-X2-01.jpg', 'http://1.bp.blogspot.com/-3GP7A_6Xv60/UGEy12xKizI/AAAAAAAAA-M/w_sbePa1y4M/s1600/nL-Mus+%252818%2529.gif', '50 tập phim hoạt hình Doremon HTV3 lồng tiếng cho màn hình 240 x 320. Thảo luận trong \'Phim - Ảnh\' bắt đầu bởi MrBe09, 24/8/14.', 0, 'công chiếu', 10, '', 0),
+(5, 'doraemon', 'tập 3', 1985, 'Hành động', 'Jeffrey Grellman ', 'http://s2.dmcdn.net/OAEnW/x240-Rfh.jpg', 'https://3.bp.blogspot.com/-iJDgZgMPkcM/U9TDMWLYlII/AAAAAAAAAQU/rMDJWr91xO4/s1600/Yeualo_net-240x320-29.jpg', 'Bộ phim kể về một chú mèo máy tên là Doraemon đến từ thế kỉ 22', 0, 'Đang công chiếu', 11, '', 1),
+(6, 'cute phô mai que', 'tập 1', 1988, 'jewru', '', 'https://kenh14cdn.com/thumb_w/600/6a098b4e34/2015/09/02/top-phim-hoat-hinh-co-doanh-thu-ty-do-trong-lich-su_185219493a-51406.jpg', 'https://tse3.mm.bing.net/th?id=OIP.gTlMPKDhc86AUXdgm8dXqgAAAA&pid=Api&P=0&w=300&h=300', 'Hình nền kute, xem ảnh hình nền kute dễ thương nhất năm; Hình nền điện thoại 320×240 được chọn với kích thước chuẩn nét, nhiều chủ đề khác nhau mà bạn có thể tìm và tải như: hình nền điện thoại phong cảnh, nếu yêu thích thiên nhiên thì có thể chọn hình nền điện thoại thiên nhiên đẹp hoang sơ này nhé.', 0, '', 0, '', 0);
 
 -- --------------------------------------------------------
 
@@ -175,7 +180,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`, `name`, `email`, `avatar`, `age`, `role_id`) VALUES
 (1, 'nguyenvana', '123456', 'nguyễn văn a', 'haivdph@gmail.com', '', '19', 0),
-(2, 'nguyenvanb', '123123', 'nguyễn văn b', 'bbb@gmail.com', '', '19', 0);
+(2, 'nguyenvanb', '123123', 'nguyễn văn b', 'bbb@gmail.com', '', '19', 0),
+(3, 'admin', '$2y$10$QYr7SvXdOguZz3dlnPxO.u0Y0Ffj6nNErwm168unZkzPoWRxR7..e', 'admin', 'haivdph07968@gmail.com', '', '', 10);
 
 -- --------------------------------------------------------
 
@@ -262,7 +268,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT cho bảng `films`
 --
 ALTER TABLE `films`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT cho bảng `follows`
@@ -298,7 +304,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT cho bảng `years`
